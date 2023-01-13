bb6d576a0da7307c8cd8727021c4f70033b1a2a4
gasparmaria <maria.eduarda.gaspar07@gmail.com>
2023-01-12 21:07:23 -0300

    Initial commit

---
+++ lib/pages/todolist_page.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/repositories/todo_repository.dart';
+import 'package:todo_list/widgets/todolist_item.dart';
+import '../models/todo.dart';
+
+class TodoListPage extends StatefulWidget {
+  TodoListPage({Key? key}) : super(key: key);
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final TextEditingController todoController = TextEditingController();
+  final TodoRepository todoRepository = TodoRepository();
+
+  List<Todo> todos = [];
+  Todo? deletedTodo;
+  int? deletedTodoPos;
+  String? errorText;
+
+  @override
+  void initState(){
+    super.initState();
+
+    todoRepository.getTodoList().then((value) {
+      setState((){
+        todos = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: todoController,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          labelText: 'Adicione uma tarefa',
+                          errorText: errorText,
+                          focusedBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0xff00d7f3),
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    ElevatedButton(
+                        onPressed: () {
+                          String text = todoController.text;
+
+                          if(text.isEmpty){
+                            setState((){
+                              errorText = 'A tarefa não pode ser vazia.';
+                            });
+                            return;
+                          }
+
+                          setState(() {
+                            Todo newTodo = Todo(
+                              title: text,
+                              dateTime: DateTime.now(),
+                            );
+                            todos.add(newTodo);
+                            errorText = null;
+                          });
+                          todoController.clear();
+                          todoRepository.saveTodoList(todos);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.all(14),
+                          backgroundColor: const Color(0xff00d7f3),
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          size: 30,
+                        ))
+                  ],
+                ),
+                const SizedBox(height: 16),
+                ListView(
+                  shrinkWrap: true,
+                  children: [
+                    for (Todo todo in todos)
+                      TodoListItem(
+                        todo: todo,
+                        onDelete: onDelete,
+                      ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                          'Você possui ${todos.length} tarefas pendentes.'),
+                    ),
+                    const SizedBox(width: 8),
+                    ElevatedButton(
+                      onPressed: deleteAllTodos,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(14),
+                        backgroundColor: const Color(0xff00d7f3),
+                      ),
+                      child: const Text('Limpar tudo'),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onDelete(Todo todo) {
+    deletedTodo = todo;
+    deletedTodoPos = todos.indexOf(todo);
+
+    setState(() {
+      todos.remove(todo);
+    });
+    todoRepository.saveTodoList(todos);
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        'Tarefa ${todo.title.toLowerCase()} excluída.',
+        style: const TextStyle(
+          color: Color(0xff060708),
+        ),
+      ),
+      backgroundColor: Colors.white,
+      duration: const Duration(seconds: 5),
+      action: SnackBarAction(
+        label: 'Desfazer',
+        textColor: const Color(0xff00d7f3),
+        onPressed: () {
+          setState(() {
+            todos.insert(deletedTodoPos!, deletedTodo!);
+          });
+          todoRepository.saveTodoList(todos);
+        },
+      ),
+    ));
+  }
+
+  void deleteAllTodos() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Limpar tudo'),
+        content: const Text('Tem certeza que deseja apagar todas as tarefas?'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            style: TextButton.styleFrom(
+              foregroundColor: const Color(0xff00d7f3),
+            ),
+            child: const Text('Cancelar'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              setState(() {
+                todos.clear();
+              });
+              todoRepository.saveTodoList(todos);
+            },
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+            child: const Text('Limpar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/todolist_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
+import '../models/todo.dart';
+
+class TodoListItem extends StatelessWidget {
+  const TodoListItem({
+    Key? key,
+    required this.todo,
+    required this.onDelete,
+  }) : super(key: key);
+
+  final Todo todo;
+  final Function(Todo) onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const DrawerMotion(),
+          extentRatio: 0.25,
+          children: [
+            SlidableAction(
+              label: 'Deletar',
+              backgroundColor: Colors.red,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(4),
+              onPressed: (context) {
+                onDelete(todo);
+              },
+            ),
+          ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: Colors.grey[200],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+                style: const TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+              Text(
+                todo.title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
